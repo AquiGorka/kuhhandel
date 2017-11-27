46a9525baf65b4c65a9eb967a5860229933d7c26
Gorka <gorka@aquigorka.com>
2017-11-27 17:01:20 +0100

    Kuhhandel: add players with unique ids

--- packages/kuhhandel/tests/index-test.js
+++ packages/kuhhandel/tests/index-test.js
@@ -1,6 +1,7 @@
 import expect from 'expect'
-
 import Kuhhandel from 'src/index'
+
+const randomPlayers = () => Math.floor(2 + Math.random() * 3)
 
 describe('Kuhhandel', () => {
   it('default to 2 players', () => {
@@ -14,5 +15,12 @@ describe('Kuhhandel', () => {
 
   it('should throw if num players if more than 5', () => {
     expect(() => new Kuhhandel({ players: 6 })).toThrow('A maximum of 5 players is allowed')
+  })
+
+  it('should instantiate players with unique ids', () => {
+    const numPlayers = randomPlayers()
+    const kh = new Kuhhandel({ players: numPlayers })
+    const set = new Set(kh.players.map(p => p.id))
+    expect(Array.from(set).length).toBe(numPlayers)
   })
 })
