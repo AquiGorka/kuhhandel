2d6a6d8376c52175c8aca0b9bfe7787d5b7f3425
Gorka <gorka@aquigorka.com>
2017-11-27 16:54:02 +0100

    Kuhhandel: add player class

--- packages/kuhhandel/src/index.js
+++ packages/kuhhandel/src/index.js
@@ -1,12 +1,41 @@
+/*
+40 Animal cards
+10 Pedigree cards (one of each animal)
+4 Rat cards
+55 money cards [10x0, 20x10, 10x50, 5x100, 5x200, 5x500]
+*/
+
+const MONEY = [
+  { value: 0 }, { value: 10 }, { value: 20 }, { value: 50 }, { value: 200 }, { value: 500 },
+]
+const ANIMALS = [
+  { animal: 'chicken', value: 10 },
+  { animal: 'goose', value: 40 },
+  { animal: 'cat', value: 90 },
+  { animal: 'dog', value: 160 },
+  { animal: 'sheep', value: 250 },
+  { animal: 'goat', value: 350 },
+  { animal: 'donkey', value: 500 },
+  { animal: 'pig', value: 650 },
+  { animal: 'cow', value: 800 },
+  { animal: 'horse', value: 1000 },
+]
+
+class Player {
+  constructor({ id }) {
+    this.id = id
+  }
+}
+
 class Kuhhandel {
-  constructor({ numPlayers = 2 } = {}) {
-    if (numPlayers < 2) {
+  constructor({ players = 2 } = {}) {
+    if (players < 2) {
       throw new Error('A minimum of 2 players is required')
     }
-    if (numPlayers > 5) {
+    if (players > 5) {
       throw new Error('A maximum of 5 players is allowed')
     }
-    this.numPlayers = numPlayers
+    this.players = Array(players).fill(0).map((item, index) => new Player({ id: index }))
   }
 }
 
--- packages/kuhhandel/tests/index-test.js
+++ packages/kuhhandel/tests/index-test.js
@@ -5,14 +5,14 @@ import Kuhhandel from 'src/index'
 describe('Kuhhandel', () => {
   it('default to 2 players', () => {
     const kh = new Kuhhandel()
-    expect(kh.numPlayers).toBe(2)
+    expect(kh.players.length).toBe(2)
   })
 
   it('should throw if num players is less than 2', () => {
-    expect(() => new Kuhhandel({ numPlayers: 1 })).toThrow('A minimum of 2 players is required')
+    expect(() => new Kuhhandel({ players: 1 })).toThrow('A minimum of 2 players is required')
   })
 
   it('should throw if num players if more than 5', () => {
-    expect(() => new Kuhhandel({ numPlayers: 6 })).toThrow('A maximum of 5 players is allowed')
+    expect(() => new Kuhhandel({ players: 6 })).toThrow('A maximum of 5 players is allowed')
   })
 })
