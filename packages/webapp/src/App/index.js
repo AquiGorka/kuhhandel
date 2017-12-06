6e8f5442bd6b64bb4b7686052e45ae22b0fc7e3a
Gorka <gorka@aquigorka.com>
2017-12-06 10:50:57 +0100

    Webapp: add board boilerplate

--- packages/webapp/src/App/index.js
+++ packages/webapp/src/App/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
 
+const Board = props => <div>Board</div>
+
 class App extends Component {
   render() {
-    return <div>Kuhhandel</div>
+    return [
+      <Board />,
+      <div>Players</div>
+    ]
   }
 }
 
