501702db5f7e5e698c43e1042bfef469d7279bc7
Gorka <gorka@aquigorka.com>
2017-12-08 09:31:08 +0100

    Game: add saving & retrieving state from local storage

--- packages/kuhhandel-webapp/src/Game/index.js
+++ packages/kuhhandel-webapp/src/Game/index.js
@@ -1,15 +1,41 @@
 import Kuhhandel from 'kuhhandel'
 import { EventEmitter } from 'events'
+import localForage from 'localforage'
+
+const KH = 'Kuhhandel'
 
 class Game extends EventEmitter {
   constructor() {
     super()
     this.kh = null
+    this.init()
   }
 
-  setup({ players }) {
+  init = async () => {
+    const state = await this.getState()
+    if (state && state.setup) {
+      this.setup({ players: state.players })
+    }
+  }
+
+  setup = async ({ players }) => {
     this.kh = new Kuhhandel({ players })
+    await this.saveState()
     this.emit('setup')
+  }
+
+  /* save & fetch from localstorage */
+  getState = async () => {
+    return await localForage.getItem(KH)
+  }
+
+  saveState = async () => {
+    if (!this.kh) {
+      throw new Error('Cannot save state if game has not been setup')
+    }
+    await localForage.setItem(KH, {
+      setup: { players: this.kh.players }
+    })
   }
 }
 
