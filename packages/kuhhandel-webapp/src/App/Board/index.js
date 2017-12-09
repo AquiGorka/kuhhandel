740938d1116de7f2e85a2190edd6c88849168287
Gorka <gorka@aquigorka.com>
2017-12-09 18:51:16 +0100

    Board: add animals

--- packages/kuhhandel-webapp/src/App/Board/index.js
+++ packages/kuhhandel-webapp/src/App/Board/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
+const Animals = ({ players }) => players.map(({ id, animals}) => 
+  <div key={id}>{'Player ' + id + ': ' + JSON.stringify(animals)}</div>)
+
 const Board = ({ game }) => [
   <div key="stack">Stack: {game.stack.length}</div>,
   <div key="currentDraw">Current draw: {JSON.stringify(game.currentDraw)}</div>,
   <div key="currentAuction">Current auction: {JSON.stringify(game.currentAuction)}</div>,
   <div key="highestOffer">Current auction highest offer: {JSON.stringify(game.highestBid)}</div>,
   <div key="canPay">Current auction can the player pay: {JSON.stringify(game.canThePlayerPay)}</div>,
-  <div key="canPayFalse">Player money: {JSON.stringify(game.cannotPayPlayerMoney)}</div>
+  <div key="canPayFalse">Player money: {JSON.stringify(game.cannotPayPlayerMoney)}</div>,
+  <Animals key="animals" players={game.players} />,
 ]
 
 export default Board
