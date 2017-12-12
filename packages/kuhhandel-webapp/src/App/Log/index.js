38fc0e973ba7cfef1f9baa31ad06a71f6c2b7bee
Gorka <gorka@aquigorka.com>
2017-12-12 09:02:01 +0100

    Log: add remove last item

--- packages/kuhhandel-webapp/src/App/Log/index.js
+++ packages/kuhhandel-webapp/src/App/Log/index.js
@@ -6,30 +6,51 @@ class Log extends Component {
 
   state = { list: [] }
 
-  async componentDidMount() {
-    const list = await localForage.getItem(KH) || []
-    this.setState({ list })
+  componentDidMount() {
+    this.props.game.on('update', this.fetch)
   }
 
   render() {
+    const { list } = this.state
+
     return [
       <div key="title">Log</div>,
       <button key="reset" onClick={this.onReset}>Reset</button>,
       <ul key="list">
-        {this.state.list.map((action, index) => {
-          return <li key={index}>Action</li>
-        })}
+        {list.concat().reverse().map((action, index) =>
+          <li key={index}>
+            <div>{list.length - index}</div>
+            <div>{action.method}</div>
+            <div>{JSON.stringify(action.payload)}</div>
+            {index === 0 && <button onClick={this.onRemove}>x</button>}
+          </li>
+        )}
       </ul>
     ]
+  }
+
+  fetch = () => {
+    // updates are sent before writing to log
+    // so we need to wait sometime before fetching
+    setTimeout(async () => {
+      const list = await localForage.getItem(KH) || []
+      this.setState({ list })
+    }, 500)
+  }
+
+  onReload = () => {
+    window.location.reload()
+  }
+
+  onRemove = async () => {
+    const list = this.state.list.slice(0, -1)
+    await localForage.setItem(KH, list)
+    this.onReload()
   }
 
   onReset = () => {
     localForage.setItem(KH, [])
     this.onReload()
-  }
-
-  onReload = () => {
-    window.location.reload()
   }
 }
 
