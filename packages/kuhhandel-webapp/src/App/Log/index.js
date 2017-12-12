910b6009d46eb9d6c3a7b212e3405160fe42ae13
Gorka <gorka@aquigorka.com>
2017-12-12 08:43:36 +0100

    Log: add reset & list placeholder

--- packages/kuhhandel-webapp/src/App/Log/index.js
+++ packages/kuhhandel-webapp/src/App/Log/index.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
 import { KH } from '../../Game'
+import localForage from 'localforage'
 
 class Log extends Component {
+
+  state = { list: [] }
+
+  async componentDidMount() {
+    const list = await localForage.getItem(KH) || []
+    this.setState({ list })
+  }
+
   render() {
-    return <div>Log</div>
+    return [
+      <div key="title">Log</div>,
+      <button key="reset" onClick={this.onReset}>Reset</button>,
+      <ul key="list">
+        {this.state.list.map((action, index) => {
+          return <li key={index}>Action</li>
+        })}
+      </ul>
+    ]
+  }
+
+  onReset = () => {
+    localForage.setItem(KH, [])
+    this.onReload()
+  }
+
+  onReload = () => {
+    window.location.reload()
   }
 }
 
