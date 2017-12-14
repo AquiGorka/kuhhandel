14354b945a4ad17ae27280d1613deeaef092dfe2
Gorka <gorka@aquigorka.com>
2017-12-14 18:01:15 +0100

    Remote control: display render data & send method with payload

--- packages/kuhhandel-remote-control/src/App/index.js
+++ packages/kuhhandel-remote-control/src/App/index.js
@@ -3,6 +3,7 @@ import qmark from 'qmark'
 import Peer from 'simple-peer'
 import GoogleURL from 'google-url'
 import promisify from 'es6-promisify'
+import { Control } from 'kuhhandel-components'
 import './App.css'
 
 let peer = null
@@ -11,7 +12,7 @@ const shorten = promisify(googleUrl.shorten.bind(googleUrl))
 
 class App extends Component {
 
-  state = { connected: false, error: false, id: null }
+  state = { connected: false, error: false, id: null, props: null }
 
   async componentDidMount() {
     const signalData = atob(qmark('signalData'))
@@ -22,11 +23,13 @@ class App extends Component {
     peer = new Peer({ trickle: false })
     peer.signal(signalData)
     peer.on('signal', this.onSignal)
+    peer.on('data', data => this.onProps(JSON.parse(data)))
     peer.on('connect', () => this.setState({ connected: true }))
+    peer.on('close', () => this.setState({ connected: false }))
   }
 
   render() {
-    const { error, connected, id } = this.state
+    const { error, connected, id, props } = this.state
 
     if (error) {
       console.log(error);
@@ -37,13 +40,33 @@ class App extends Component {
       return <div>{id}</div>
     }
 
-    if (connected) {
-      //
+    if (props) {
+      const { id } = props
+      const overloadedProps = {
+        ...props,
+        onDraw: () => this.onSend({ method: 'draw', payload: id}),
+        onAuctionStart: () => this.onSend({ method: 'auctionStart', payload: id }),
+        onAuctionClose: () => this.onSend({ method: 'auctionClose'}),
+        onAuctionOffer: value => this.onSend({ method: 'auctionOffer', payload: { playerId: id, value }}),
+        onExchange: payload => this.onSend({ method: 'exchange', payload }),
+        onBuyBack: payload => this.onSend({ method: 'buyBack', payload }),
+        onCowTradeStart: opts => this.onSend({ method: 'cowTradeStart', payload: { ...opts, initiatorId: id }}),
+        onCowTradeRespond: payload => this.onSend({ method: 'cowTradeRespond', payload }),
+      }
+      return <Control {...overloadedProps} />
     }
 
     return <div>
       Remote Control
     </div>
+  }
+
+  onProps = props => {
+    this.setState({ props })
+  }
+
+  onSend = opts => {
+    peer.send(JSON.stringify(opts))
   }
 
   onSignal = async data => {
