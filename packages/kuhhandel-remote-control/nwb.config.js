b49735e39610cfcc4e91bd7836bf73a86f978b95
Gorka <gorka@aquigorka.com>
2017-12-14 14:16:34 +0100

    Remote control: setup remote control to return signal data

--- packages/kuhhandel-remote-control/nwb.config.js
+++ packages/kuhhandel-remote-control/nwb.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  type: 'react-app'
+  type: 'react-app',
+  webpack: {
+    define: {
+      GOOGLE_APIKEY: JSON.stringify(process.env.GOOGLE_APIKEY),
+      HOST_URL: JSON.stringify(process.env.HOST_URL)
+    },
+    extra: {
+      node: {
+        console: true,
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+      }
+    }
+  }
 }
--- packages/kuhhandel-remote-control/src/App/index.js
+++ packages/kuhhandel-remote-control/src/App/index.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react'
+import qmark from 'qmark'
+import Peer from 'simple-peer'
+import GoogleURL from 'google-url'
+import promisify from 'es6-promisify'
 import './App.css'
 
+let peer = null
+const googleUrl = new GoogleURL({ key: GOOGLE_APIKEY })
+const shorten = promisify(googleUrl.shorten.bind(googleUrl))
+const expand = promisify(googleUrl.expand.bind(googleUrl))
+
 class App extends Component {
+
+  state = { connected: false, error: false, id: null }
+
+  async componentDidMount() {
+    const signalData = atob(qmark('signalData'))
+    if (!signalData) {
+      this.setState({ error: 'No signal data was provided' })
+      return
+  }
+    peer = new Peer({ trickle: false })
+    peer.signal(signalData)
+    peer.on('signal', this.onSignal)
+  }
+
   render() {
+    const { error, connected, id } = this.state
+
+    if (error) {
+      console.log(error);
+      return <div>There has been an error, please try again.</div>
+    }
+
+    if (!connected && id) {
+      return <div>{id}</div>
+    }
+
+    if (connected) {
+      //
+    }
+
     return <div>
       Remote Control
     </div>
+  }
+
+  onSignal = async data => {
+    const shortUrl = await shorten(`${HOST_URL}?connect=${btoa(JSON.stringify(data))}`)
+    this.setState({ id: shortUrl.substr(15) })
   }
 }
 
