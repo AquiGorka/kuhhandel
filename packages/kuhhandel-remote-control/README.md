4cb581f357c7598cc34661c268c576c5ff4bdca3
Gorka <gorka@aquigorka.com>
2017-12-14 11:19:36 +0100

    Kuhhandel: add kuhhandel remote control webapp package boilerplate

---
+++ packages/kuhhandel-remote-control/README.md
@@ -0,0 +1,2 @@
+# Kuhhandel Remote Control
+
---
+++ packages/kuhhandel-remote-control/src/App/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return <div>
+      Remote Control
+    </div>
+  }
+}
+
+export default App
